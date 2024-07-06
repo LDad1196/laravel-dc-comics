7e8985ea2bb361d0b1f1f90fc8bed829164acfd7
Lorenzo D'Addazio <ldaddazio11@gmail.com>
2024-07-06 10:52:42 +0200

    Collegato progetto a mysql

--- database/seeders/ComicsTableSeeder.php
+++ database/seeders/ComicsTableSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use app\Models\Comics;
+use App\Models\Comic;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
